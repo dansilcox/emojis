70e4ce38dd3b241067383d36d40fcf062e1c84d4
Dan Silcox <dan.silcox@ctt.co.uk>
2019-08-09 13:27:46 +0100

    Fix line endings issue

--- update-readme.sh
+++ update-readme.sh
@@ -2,7 +2,7 @@ IMG_LIST=$(find ./ -regex ".*\.\(jpg\|gif\|png\|jpeg\)")
 echo "Found images: $IMG_LIST"
 echo "# A few emoji favourites" > readme-new.md
 for FILE in $IMG_LIST; do
-    echo "![$FILE]($FILE)\r\n" >> readme-new.md
+    printf "![$FILE]($FILE)\n" >> readme-new.md
 done
 mv readme-new.md README.md
 echo "Readme updated with previews of the above!"
